0e93683a1a9cc1be9683cdc723203e111585f489
robertuptc <robertuptc@gmail.com>
2024-06-04 12:01:57 -0400

    python portion finished

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,15 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num):
+	for b_num in reversed(range(num)):
+		if b_num > 0:
+			print(f"{b_num + 1} bottles of beer on the wall, {b_num + 1} bottles of beer.")
+			if b_num > 1:
+				print(f"Take one down and pass it around, {b_num} bottles of beer on the wall.")
+			else: 
+				print(f"Take one down and pass it around, {b_num} bottle of beer on the wall.")
+		else:
+			print(f"{b_num + 1} bottle of beer on the wall, {b_num + 1} bottle of beer.")
+			print(f"Take one down and pass it around, no more bottles of beer on the wall.")
+			print("No more bottles of beer on the wall, no more bottles of beer.\n\
+Go to the store and buy some more, 99 bottles of beer on the wall.")
 
-
-bottle_song()
+bottle_song(5)
